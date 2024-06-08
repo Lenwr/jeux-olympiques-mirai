617398551a4892b1c70c67781064acccad3a64a1
Lenwr <saidabou.lateam@gmail.com>
2024-06-08 13:48:33 +0200

    first commit

---
+++ pages/api/submit.ts
@@ -0,0 +1,56 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {google} from "googleapis"
+
+type SheetForm = {
+    date: any,
+    name: string,
+    chassis: string
+}
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST') {
+        return res.status(405).send({message: 'Seul les requetes POST sont autorisés'})
+    }
+    const body = req.body as SheetForm
+
+    try {
+        const auth = new google.auth.GoogleAuth(
+            {
+                credentials: {
+                    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+                },
+                scopes: [
+                    'https://www.googleapis.com/auth/drive',
+                    'https://www.googleapis.com/auth/drive.file',
+                    'https://www.googleapis.com/auth/spreadsheets'
+                ]
+            }
+        );
+        const sheets = google.sheets({
+            auth,
+            version: 'v4'
+        });
+        const response = await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            range: 'A1:C1',
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+                values: [
+                   [ body.date,
+                    body.name,
+                    body.chassis]
+                ]
+            }
+        })
+        return res.status(200).json({
+            data: response.data
+        })
+
+    } catch (e) {
+        console.error(e)
+        return res.status(500).send({message: 'il y a une erreur'})
+    }
+}
